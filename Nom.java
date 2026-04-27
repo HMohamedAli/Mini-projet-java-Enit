d21b586286af7475350f910400a9d705a6467f4d
wissem toujani <wissem.toujani@etudiant-enit.utm.tn>
2026-04-27 21:19:46 +0100

    general project structure

---
+++ Nom.java
@@ -0,0 +1,16 @@
+public class Nom {
+    public String id;
+    public  String nomoriginal
+    public  Nom nomtraitee 
+    public String getId() {
+        return id;
+    }
+    public String getNomoriginal() {
+        return nomoriginal;
+    }
+    public String getNomtraitee() {
+        return nomtraitee;
+    }
+
+    
+}
