d5bd39fe4f1845a2b414b7a911689cc0e72b2133
wissem toujani <wissem.toujani@etudiant-enit.utm.tn>
2026-05-03 22:26:33 +0100

    Fixed project structure

--- EnMajuscules.java
+++
@@ -1,9 +0,0 @@
-
-public class EnMajuscules extends PretraiteurNom {
-
-    @Override
-    public Nom traiter(Nom b) {
-        if (b == null) return null;
-        return new Nom(b.getId(), b.getNom().toUpperCase());
-    }
-}
--- SuppressionEspaces.java
+++
@@ -1,9 +0,0 @@
-public class SuppressionEspaces extends PretraiteurNom {
-
-    @Override
-    public Nom traiter(Nom nom) {
-        if (nom == null) return null;
-        String s = nom.getNom().trim().replaceAll("\\s+", " ");
-        return new Nom(nom.getId(), s);
-    }
-}
